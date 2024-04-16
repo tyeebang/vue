a39cc2db2618cc9645518be060f0686ada89b62f
tyeebang <111326879+tyeebang@users.noreply.github.com>
2024-04-16 15:08:30 +0900

    v-for

--- vueJS/first/src/App.vue
+++ vueJS/first/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
+    <nav class="menu">
+        <a v-for="(menu, i) in ['Home', 'Product', 'About us']" :key="i" href="#"> {{ menu }} </a>
+        <!-- <a href="#">Product</a>
+        <a href="#">About us</a> -->
+    </nav>
 
-    <div>
+    <!-- <div>
         <h3> {{ product[0] }} </h3>
          <p>{{ price[0] }}만원</p>
     </div>
@@ -12,6 +16,12 @@
     <div>
         <h3> {{ product[2] }} </h3>
          <p>{{ price[2] }}만원</p>
+    </div> -->
+
+    <div v-for="(prtd, i) in product" :key="i">
+        <h3> {{ prtd }} </h3>
+        <p> {{ price[i] }} </p>
+        <button @click = reportAdd(i)> 허위 매물 신고 </button> <span>신고수 : {{ report[i] }}</span>
     </div>
 </template>
 
@@ -22,11 +32,19 @@
         data() {
             return {
                 product : [
-                    '역삼동 원룸', '마포구 원룸', '천호동 원룸'
+                    '역삼동 원룸', '마포구 원룸', '천호동 원룸', '은행동 원룸'
                 ], 
                 price : [
-                    50, 70, 60
+                    50, 70, 60, 40
+                ],
+                report : [
+                    0, 0, 0, 0
                 ]
+            }
+        },
+        methods: {
+            reportAdd(i) {
+                this.report[i]++
             }
         },
         components: {
@@ -42,6 +60,17 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
 }
+
+    .menu {
+        background-color: darkslateblue;
+        padding: 15px;
+        border-radius: 5px;
+    }
+
+    .menu a {
+        color: white;
+        padding: 10px;
+        text-decoration: none;
+    }
 </style>
