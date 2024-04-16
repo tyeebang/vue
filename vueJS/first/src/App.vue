94fa31679f3cf21c719574bd397591d1caa1d0f9
tyeebang <111326879+tyeebang@users.noreply.github.com>
2024-04-16 16:44:08 +0900

    end?

--- vueJS/first/src/App.vue
+++ vueJS/first/src/App.vue
@@ -1,51 +1,38 @@
 <template>
+
     <nav class="menu">
-        <a v-for="(menu, i) in ['Home', 'Product', 'About us']" :key="i" href="#"> {{ menu }} </a>
-        <!-- <a href="#">Product</a>
-        <a href="#">About us</a> -->
+        <a href="#">Home</a>
+        <a href="#">Product</a>
+        <a href="#">About us</a>
     </nav>
 
-    <!-- <div>
-        <h3> {{ product[0] }} </h3>
-         <p>{{ price[0] }}만원</p>
-    </div>
-    <div>
-        <h3> {{ product[1] }} </h3>
-         <p>{{ price[1] }}만원</p>
-    </div>
-    <div>
-        <h3> {{ product[2] }} </h3>
-         <p>{{ price[2] }}만원</p>
-    </div> -->
-
-    <div v-for="(prtd, i) in product" :key="i">
-        <h3> {{ prtd }} </h3>
-        <p> {{ price[i] }} </p>
-        <button @click = reportAdd(i)> 허위 매물 신고 </button> <span>신고수 : {{ report[i] }}</span>
+    <div v-for="(prtd, i) in roomData" :key="i">
+        <img :src="prtd.image" alt="room">
+        <h3 @click="isModalTrue[i] = true"> {{ prtd.title }} </h3>
+        <p> {{ prtd.price }} </p>
+        <section class="modal" v-if="isModalTrue[i]">
+            <div>
+                <h3> {{ prtd.title }} </h3>
+                <p> 상세 페이지 내용임 </p>
+                <button @click="isModalTrue[i] = false">X 닫기</button>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+    import {roomData} from "./assets/data.js";
 
     export default {
         name: 'App',
         data() {
             return {
-                product : [
-                    '역삼동 원룸', '마포구 원룸', '천호동 원룸', '은행동 원룸'
-                ], 
-                price : [
-                    50, 70, 60, 40
-                ],
-                report : [
-                    0, 0, 0, 0
-                ]
+                roomData, 
+                isModalTrue : [false, false, false, false, false]
             }
         },
         methods: {
-            reportAdd(i) {
-                this.report[i]++
-            }
+
         },
         components: {
 
@@ -55,12 +42,12 @@
 
 <style>
     #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
     .menu {
         background-color: darkslateblue;
@@ -72,5 +59,46 @@
         color: white;
         padding: 10px;
         text-decoration: none;
+    }
+
+    img {
+        width: 50%;
+        margin: 30px;
+    }
+
+    body {
+        position: relative;
+    }
+
+    .modal {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, .5);
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .modal div {
+        width: 600px;
+        height: 200px;
+        border-radius: 15px;
+        background-color: #fff;
+        z-index: 100000;
+    }
+
+    .modal button {
+        width: 100px;
+        height: 40px;
+        margin-left: 450px; 
+        margin-top: 40px;
+        border: none;
+        background-color: #333;
+        color: #fff;
+        border-radius: 15px;
+        cursor: pointer;
     }
 </style>
